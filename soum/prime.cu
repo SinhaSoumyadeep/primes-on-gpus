639b38d2ce634c8066db62bd6cb33cf5dfd3c0b2
Mayukh2011 <soumyadeep.sinha2011@gmail.com>
2018-03-22 21:14:02 -0400

    modifications

--- soum/prime.cu
+++ soum/prime.cu
@@ -3,19 +3,20 @@
 #include<math.h>
 #include<cuda.h>
 
-__global__ void prime_generator(int *input,int *prime_list,int *total_input)
+__global__ void prime_generator(int *input,int *prime_list,int *total_input,int *seed)
 {
 
 	int i= blockIdx.x * blockDim.x + threadIdx.x;
 	int primeno= prime_list[i];
-
-	for(int k=10;k<total_input[0];k++)
+	
+	for(int k=seed[0];k<total_input[0];k++)
 	{
 		if(k%primeno==0)
 		{
 			input[k]=1;
-				
+		
 		}
+	
 
 	}
 	
@@ -25,9 +26,9 @@ __global__ void prime_generator(int *input,int *prime_list,int *total_input)
 
 int main()
 {
-	int total_input=100;
+	int total_input=1e6;
 	int *input;
-	int n= 10 ;// seed prime list.
+	int n= 1000 ;// seed prime list.
 	int *primelist;
 	input=(int *)malloc(total_input*sizeof(int));
 	primelist=(int *)malloc(total_input*sizeof(int));
@@ -71,32 +72,42 @@ int *d_input;
 int *d_prime_list;
 int *h_pl;
 int *d_total_input;
+int *d_seed;
 
-h_pl=(int *)malloc(i*sizeof(int));
+h_pl=(int *)malloc(total_input*sizeof(int));
 
 cudaMalloc(&d_input,total_input*sizeof(int));
-
-
-
 cudaMalloc(&d_prime_list,total_input*sizeof(int));
 cudaMalloc(&d_total_input,sizeof(int));
+cudaMalloc(&d_seed,sizeof(int));
 
-
-cudaMemcpy(d_input,input,total_input*sizeof(int),cudaMemcpyHostToDevice);
-cudaMemcpy(d_prime_list,primelist,i*sizeof(int),cudaMemcpyHostToDevice);
 cudaMemcpy(d_total_input,&total_input,sizeof(int),cudaMemcpyHostToDevice);
+cudaMemcpy(d_input,input,total_input*sizeof(int),cudaMemcpyHostToDevice);
 
-prime_generator<<<1,4>>>(d_input,d_prime_list,d_total_input);
 
-cudaMemcpy(h_pl,d_prime_list,i*sizeof(int),cudaMemcpyDeviceToHost);
+
+cudaMemcpy(d_prime_list,primelist,total_input*sizeof(int),cudaMemcpyHostToDevice);
+cudaMemcpy(d_seed,&n,sizeof(int),cudaMemcpyHostToDevice);
+
+prime_generator<<<1,1000>>>(d_input,d_prime_list,d_total_input,d_seed);
+
+cudaMemcpy(h_pl,d_prime_list,total_input*sizeof(int),cudaMemcpyDeviceToHost);
 cudaMemcpy(input,d_input,total_input*sizeof(int),cudaMemcpyDeviceToHost);
+
 
 for(int p=2;p<total_input;p++)
 {
-	if(input[p]==1)
+ 	if(input[p]==1)
 	continue;
         		
-	printf(" %d\n",p);
+	primelist[i]=p;       
+	i++;
+}		
+	
+for(int p=0;p<total_input;p++)
+{
+	printf("%d\n",primelist[p]);
+
 }
 
 
