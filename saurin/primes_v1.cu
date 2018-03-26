bbadc8131046e07118bd997e840a7f4fab4980c8
shahsaurin <sourin.shah@gmail.com>
2018-03-25 20:09:55 -0400

    Primes upto 10^6 verified, timing the performance of CPU and GPU added

--- saurin/primes_v1.cu
+++ saurin/primes_v1.cu
@@ -1,25 +1,34 @@
 #include <stdio.h>
+#include <time.h>
 
 int generateInitialPrimes(int *intialTempArray, int **PL, int initialPrimesRange);
 __global__ void calcPrimes(int *d_IL, int *d_PL, int numOfPrimes, int lenInputList);
 
-#define LEN_IL 10000
-#define LEN_INITIAL_PRIMES 100
+#define LEN_IL 1000000
+#define LEN_INITIAL_PRIMES 1000
 #define THREADS_PER_BLOCK 32
 
 int main() {
 	int *IL = NULL, *PL = NULL, *tempPL = NULL;
 	int *d_IL = NULL, *d_PL = NULL;
+	cudaEvent_t start, stop;
+	float time;
+	cudaEventCreate(&start);
+	cudaEventCreate(&stop);
+	clock_t t;
 	//int count = 0;
 
+	t = clock();
 	int numOfInitialPrimes = generateInitialPrimes(tempPL, &PL, LEN_INITIAL_PRIMES);
+	t = clock() - t;
+	double time_taken = ((double)t)/CLOCKS_PER_SEC; 			// in seconds
 
 	// Print the initial range of primes calculated in the CPU, which will be passed to the GPU:
 	printf("\nThe initial primes calculated are:\n");
 	for(int i=0; i < numOfInitialPrimes; i++) {
 		printf("%d  ", PL[i]);
 	}
-	printf("\n\nCount of initial primes = %d\n\n", numOfInitialPrimes);
+	printf("\nNumber of initial primes = %d\n\n", numOfInitialPrimes);
 
 	// Space for host copies:
 	IL = (int*) malloc(LEN_IL * sizeof(int));
@@ -42,8 +51,12 @@ int main() {
 	cudaMemcpy(d_IL, IL, size_IL, cudaMemcpyHostToDevice);
 	cudaMemcpy(d_PL, PL, size_PL, cudaMemcpyHostToDevice);			
 
-	// Launching the kernel:
+	// Launching the kernel and measuring the time taken:
+	cudaEventRecord(start, 0);
 	calcPrimes<<<(numOfInitialPrimes/THREADS_PER_BLOCK) + 1, THREADS_PER_BLOCK>>> (d_IL, d_PL, numOfInitialPrimes, LEN_IL);
+	cudaEventRecord(stop, 0);
+	cudaEventSynchronize(stop);
+	cudaEventElapsedTime(&time, start, stop);
 
 	// Space allocated to store the modified form of input array, with marking for prime and non-prime:
 	int *result = (int*) malloc(size_IL);
@@ -52,7 +65,7 @@ int main() {
 	cudaMemcpy(result, d_IL, size_IL, cudaMemcpyDeviceToHost);
 
 	// Extract indexes of primes in 'result' to get the actual new prime numbers:
-	printf("New Primes List:\n");
+	printf("********* New Primes List **********\n");
 	int *newPrimes = (int*)malloc(LEN_IL / 4 * sizeof(int));			// Arbitrary size; which is '1/4th' of numbers list size
 	int newPrimesCount = 0;
 	for(int i=LEN_INITIAL_PRIMES; i<LEN_IL; i++) {
@@ -63,8 +76,10 @@ int main() {
 			printf("%d  ", i);
 		}
 	}
-	printf("\nNumber of new primes found = %d\n\n", newPrimesCount);
+	printf("\n\nNumber of new primes found = %d\n\n", newPrimesCount);
 
+	printf("Time taken to find initial primes on CPU = %f ms\n", time_taken * 1000);
+	printf("Parallel Job time for current iteration = %f ms\n\n", time);
 
 	// Free memory:
 	cudaFree(d_IL);
