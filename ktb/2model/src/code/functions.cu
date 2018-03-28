b0decedfce8c0767773a268a694da67de35bb59f
Kaustubh Shivdikar <kaustubhcs07@gmail.com>
2018-03-28 02:18:24 -0400

    Code Update

--- ktb/2model/src/code/functions.cu
+++ ktb/2model/src/code/functions.cu
@@ -5,26 +5,6 @@ using namespace std;
 
 #define WARNINGS 0
 
-/*
- struct TheJob {
- long int id = -1; // Will be unique per job
- long int required_cores = -1;
- long int duration = -1;
- long int duration_left = -1;
- bool valid = false;
- };
- 
- struct CompNode {
- long int id = -1;
- long int node_number = -1;
- long int total_cores= -1;
- long int used_cores = -1;
- long int free_cores = -1;
- TheJob NodeJob[NODE_MAX_JOBS]; // All Zero Initially
- long int node_job_queue = 0;
- 
- };
- */
 
 
  inline void gpuAssert(cudaError_t code, const char *file, int line, bool abort=true)
