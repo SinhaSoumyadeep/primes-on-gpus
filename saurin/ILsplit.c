30f1ffe7700633b4bc8e5517240794bf174a93c0
shahsaurin <sourin.shah@gmail.com>
2018-03-31 15:55:54 -0400

    Some stuff modified

--- saurin/ILsplit.c
+++ saurin/ILsplit.c
@@ -2,14 +2,15 @@
 typedef unsigned long long int dataType;
 
 
-void kernelLauncher(int gpus, int gpu_sel, dataType ILsize, dataType* PL, dataType PL_len, dataType* d_startPrimelist) {
+void kernelLauncher(int gpus, int gpu_sel, dataType IL_len, dataType* PL, dataType PL_len) {
 	int *d_IL = NULL, *d_PL = NULL;
 
 	// Select the device:
 	cudaSetDevice(gpu_sel);
 
 	// Calculate memory sizes required:
-	int splitILsize = (ILsize / gpus / sizeof(dataType)); 				// Confirm during code integration whether a '+1' is needed in the end.
+	dataType elementsPerSplit = IL_len / gpus;
+	dataType splitILsize = (elementsPerSplit / (sizeof(dataType) * 8)); 				// Confirm during code integration whether a '+1' is needed in the end.
 	dataType PLsize = PL_len * sizeof(dataType);
 
 	// Space for device copies:
@@ -19,8 +20,11 @@ void kernelLauncher(int gpus, int gpu_sel, dataType ILsize, dataType* PL, dataTy
 	// Copy the data to the device (GPU):
 	cudaMemcpy(d_PL, PL, size_PL, cudaMemcpyHostToDevice);
 
+	// Calculate the start value of I/P list for kernel of current GPU:
+	dataType d_startPrimelist = gpu_sel * elementsPerSplit;							// Datatype conflict
+
 	// Launch the GPU kernel:
-	prime_generator<<<(PL_len/THREADS_PER_BLOCK) + 1 , THREADS_PER_BLOCK>>>(d_IL, d_PL, d_startPrimelist, splitILsize, PL_len);
+	prime_generator<<<(PL_len/THREADS_PER_BLOCK) + 1 , THREADS_PER_BLOCK>>>(d_IL, d_PL, d_startPrimelist, splitILsize, elementsPerSplit);
 
 }
 
@@ -28,7 +32,7 @@ void kernelLauncher(int gpus, int gpu_sel, dataType ILsize, dataType* PL, dataTy
 
 
 /* NOTES:
-1) Finalize the function parameters. They vary across APIs. 
+1) Finalize the function parameters. They vary across APIs. (kernel launcher)
 
 
 */
