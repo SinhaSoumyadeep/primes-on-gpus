382c8e88b3ead86ae8e89cd2de088a411c12e18b
shahsaurin <sourin.shah@gmail.com>
2018-03-31 19:04:34 -0400

    Round-off gpu cores to power of 2 and minor changes

--- saurin/ILsplit.c
+++ saurin/ILsplit.c
@@ -2,14 +2,24 @@
 typedef unsigned long long int dataType;
 
 
+// Launch the kernel:
+
 void kernelLauncher(int gpus, int gpu_sel, dataType IL_len, dataType* PL, dataType PL_len) {
+
+	// Round off the number of GPUs to the next lower power of 2:
+	for(int i=0; i<10; i++) {
+		if(pow(2,i) > gpus) 
+			gpus = pow(2, i-1);
+			break;
+	}
+
 	int *d_IL = NULL, *d_PL = NULL;
 
 	// Select the device:
 	cudaSetDevice(gpu_sel);
 
 	// Calculate memory sizes required:
-	dataType elementsPerSplit = IL_len / gpus;
+	dataType elementsPerSplit = IL_len / gpus;											// WARNING: 'gpus' should be a power of 2 (code added for this check)
 	dataType splitILsize = (elementsPerSplit / (sizeof(dataType) * 8)); 				// Confirm during code integration whether a '+1' is needed in the end.
 	dataType PLsize = PL_len * sizeof(dataType);
 
@@ -21,7 +31,7 @@ void kernelLauncher(int gpus, int gpu_sel, dataType IL_len, dataType* PL, dataTy
 	cudaMemcpy(d_PL, PL, size_PL, cudaMemcpyHostToDevice);
 
 	// Calculate the start value of I/P list for kernel of current GPU:
-	dataType d_startPrimelist = gpu_sel * elementsPerSplit;							// Datatype conflict
+	dataType d_startPrimelist = gpu_sel * elementsPerSplit;								// Datatype conflict
 
 	// Launch the GPU kernel:
 	prime_generator<<<(PL_len/THREADS_PER_BLOCK) + 1 , THREADS_PER_BLOCK>>>(d_IL, d_PL, d_startPrimelist, splitILsize, elementsPerSplit);
@@ -30,9 +40,6 @@ void kernelLauncher(int gpus, int gpu_sel, dataType IL_len, dataType* PL, dataTy
 
 
 
-
 /* NOTES:
 1) Finalize the function parameters. They vary across APIs. (kernel launcher)
-
-
 */
