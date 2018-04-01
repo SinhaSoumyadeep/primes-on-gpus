032f4db2a845c6d451750e05262c6adc1aa71a49
Kaustubh Shivdikar <kaustubhcs07@gmail.com>
2018-04-01 03:16:44 -0400

    Code Update

--- ktb/2model/src/code/primes.cu
+++ ktb/2model/src/code/primes.cu
@@ -45,7 +45,7 @@ if (tid < *d_number_of_primes) {
 void *one_iteration(void *tid) {
     long gpu_id = (long) tid; // Dont use tid, Use gpu_id instead
     if (DEBUG >= 1) {
-        cout << "GPU Handler: " << gpu_id << endl;
+        cout << "Launched GPU Handler: " << gpu_id << endl;
     }
 
     cudaEvent_t start, stop;
