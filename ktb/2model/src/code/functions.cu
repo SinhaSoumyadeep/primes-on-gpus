cede80c9f71c2c4f5d61b115cb8200a053130d40
Kaustubh Shivdikar <kaustubhcs07@gmail.com>
2018-04-02 22:49:20 -0400

    Code Update

--- ktb/2model/src/code/functions.cu
+++ ktb/2model/src/code/functions.cu
@@ -134,7 +134,7 @@ void kernelLauncher(int gpu_id) {
     int *result = (int*) malloc(blocksFor_splitIL*sizeof(int));
 cout << "KTB: " << blocksFor_splitIL*sizeof(int) << endl;
     // Copy the result back to the host:
-    gpuErrchk( cudaMemcpy(result, d_IL, blocksFor_splitIL*(unsigned long long int)sizeof(int), cudaMemcpyDeviceToHost) );
+    gpuErrchk( cudaMemcpy(result, d_IL, blocksFor_splitIL*(uint64_cu)sizeof(int), cudaMemcpyDeviceToHost) );
 
 /*  ********** DECODING: NOT WORKING FOR NOW **************
     for(uint64_cu i=0; i<blocksFor_splitIL; i++) {
