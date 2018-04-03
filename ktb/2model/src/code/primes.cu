28dfa9dce47b335a08d1d98630dfaad4898d8110
Kaustubh Shivdikar <kaustubhcs07@gmail.com>
2018-04-02 22:26:10 -0400

    Code Update

--- ktb/2model/src/code/primes.cu
+++ ktb/2model/src/code/primes.cu
@@ -22,24 +22,25 @@ __global__ void prime_generator(int* d_input_list, uint64_cu* d_prime_list, uint
  
 uint64_cu tid = (blockIdx.x*blockDim.x) + threadIdx.x;
 
-uint64_cu primes= d_prime_list[tid];
-if(tid< d_number_of_primes[0])
-printf("%d ---->  %llu\n",tid,primes);
-/*
+//uint64_cu primes= d_prime_list[tid];
+/*if(tid< d_number_of_primes[0])
+printf("%d ---->  %llu\n",tid,primes);*/
+
      
 if (tid < *d_number_of_primes) {
     //printf("Kaustubh\n");
     uint64_cu primes=d_prime_list[tid];
-    for(uint64_cu i=0;i<=d_total_inputsize[0];i++) { // Added less than eual to here.
+    for(uint64_cu i=0;i<d_total_inputsize[0];i++) { // Added less than eual to here.
         uint64_cu bucket= i/(WORD);
         uint64_cu setbit= i%(WORD);
         uint64_cu number=d_startPrimelist[0]+i;
+        printf("THE NUMBER %llu IS BEING DIVIDED BY %llu",number,primes);
         if(number%primes==0) {
             //printf("%llu is divisible by %llu \n", number,primes);
             d_input_list[bucket]=d_input_list[bucket]| 1U<<setbit;
             }
         }
-    } */
+    } 
 }
 
 
